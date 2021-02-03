6a5ba288c4baf318a349375f7e4542d4994d6359
Andrew <andrewras93@gmail.com>
2021-02-03 12:38:39 +0100

    Covered arrays

--- main.js
+++ main.js
@@ -31,3 +31,26 @@ console.log(s.toLowerCase());
 console.log(s.substring(0, 5)); // Trækker noget af stringen fra, starter ved 0 og slutter ved 5, så der kommer kun til at stå Hello (med mellemrum).
 console.log(s.substring(0, 5).toUpperCase());
 console.log(s.split('')); // Laver et array med 11 'felter' og hvert bogstav i sit eget felt.
+
+// Arrays - variables that hold multiple values
+
+const numbers = new Array(1,2,3,4,5); // 'new' betyder det er en constructer.
+const fruits = ['apples', 'oranges', 'pears'];
+
+console.log(numbers);
+console.log(fruits);
+
+console.log(fruits[1]); // Arrays starter altid fra 0 (apples = 0, oranges = 1, pears = 2).
+
+fruits[3] = 'grapes';
+fruits.push('mangos'); // Indsætter værdien til sidst i arrayet.
+
+fruits.unshift('strawberries'); // Indsætter værdien i starten af arrayet.
+
+fruits.pop(); // Fjerner den sidste værdi i arrayet.
+
+console.log(Array.isArray(fruits));
+
+console.log(fruits);
+
+console.log(fruits.indexOf('oranges'));
